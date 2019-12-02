0261c5ac60da7ba31949a96ec935a725bd559c69
Alexandr Kolisetsky <skolisetsky@yahoo.com>
2019-12-02 15:36:58 +0200

    Updates

---
+++ precache-manifest.81c0924329333d31408d20e7b3ee913f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397d8d90b77086225289c83516ddaefe",
+    "url": "/lifecircules-sync-crude/index.html"
+  },
+  {
+    "revision": "50344ed60da79beb39ff",
+    "url": "/lifecircules-sync-crude/static/css/main.07090cd0.chunk.css"
+  },
+  {
+    "revision": "5bfaa8252eedb88c1b2b",
+    "url": "/lifecircules-sync-crude/static/js/2.2426b2f2.chunk.js"
+  },
+  {
+    "revision": "50344ed60da79beb39ff",
+    "url": "/lifecircules-sync-crude/static/js/main.e41df290.chunk.js"
+  },
+  {
+    "revision": "d99793148ecd29b33d8e",
+    "url": "/lifecircules-sync-crude/static/js/runtime-main.63e1ed4f.js"
+  }
+]);
